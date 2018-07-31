8ec310c8f9b823a1ad392eb45df74851ddec06c5
Justin McKay <1891757+jcmckay@users.noreply.github.com>
2018-07-31 10:58:32 -0700

    reveal algorithm working

--- src/containers/Board.js
+++ src/containers/Board.js
@@ -1,19 +1,22 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {revealAdjacent, findAdjacentMines, validDiffs} from '../helpers/board-helpers';
 
 const Board = ({board}) => {
-    return(
-      <div id="board">
-        <div className="cells">
-          {board.map((cell) => {
-            return <div className="cell"
-                        data-type={cell.type}
-                        onClick={e => guess(e, cell.id, board)}
-                   >{cell.code}</div>
-          })}
-        </div>
+  return(
+    <div id="board">
+      <div className="cells">
+        {board.map((cell) => {
+          return <div
+                      id={`CellId-${cell.id}`}
+                      className={`cell ${cell.type}`}
+                      data-type={cell.type}
+                      onClick={e => guess(e, cell.id, board)}
+                  >{cell.code}</div>
+        })}
       </div>
-    );
+    </div>
+  );
 }
 
 const guess = (e, id, board) => {
@@ -22,92 +25,19 @@ const guess = (e, id, board) => {
 
   if(type === 'mine') {
     console.log('GAME OVER!');
+    document.querySelectorAll('.cells .cell').forEach(cell => cell.style.backgroundColor = '#fff');
+    const mines = document.querySelectorAll('.cells .cell.mine');
+    mines.forEach(cell => {cell.style.backgroundColor = 'red'; cell.style.color = '#fff'});
+    return;
   }
-
-  // CHECK N , NE, E, SE, S, SW, W for bombs
-  // NW, N, NE => id-10, id-9, id-8
-  // W, E      => id-1, id+1
-  // SW, S, SW => id+8, id+9, id+10
 
   const mineCount = findAdjacentMines(id, board);
 
   if(mineCount === 0) {
-
-  } else {
-    e.target.textContent = mineCount;
+    // find and active all the adjacent elements
+    revealAdjacent(id, validDiffs(id), board);
   }
 };
-
-const findAdjacentMines = (id, board) => {
-  let total   = 0,
-      diffs = validDiffs(id);
-  console.log(diffs);
-  for(let diff of diffs) {
-    let cell = board[id+diff-1];
-
-    console.log(id, cell);
-    if(cell && cell.type === 'mine') {
-      total++;
-    }
-  }
-
-  return total;
-};
-
-const validDiffs = (id) => {
-  let diffs = [-10, -9, -8, -1, 1, 8, 9, 10];
-
-  // Top Left Corner
-  if(id === 1) {
-    diffs[0] = null;
-    diffs[1] = null;
-    diffs[2] = null;
-    diffs[3] = null;
-    diffs[5] = null;
-  }
-
-  // Top Right Corner
-  if(id === 9) {
-    diffs[0] = null;
-    diffs[1] = null;
-    diffs[2] = null;
-    diffs[4] = null;
-    diffs[7] = null;
-  }
-
-  if(id === 73) {
-    diffs[0] = null;
-    diffs[3] = null;
-    diffs[5] = null;
-    diffs[6] = null;
-    diffs[7] = null;
-  }
-
-  // Bottom Right Corner
-  if(id === 81) {
-    diffs[2] = null;
-    diffs[4] = null;
-    diffs[5] = null;
-    diffs[6] = null;
-    diffs[7] = null;
-  }
-
-  // Left side
-  if([1,10,19,28,37, 46, 55, 64, 73].includes(id)) {
-    diffs[3] = null;
-    diffs[0] = null;
-    diffs[5] = null;
-  }
-
-  // Right Side
-  if(id % 9 === 0) {
-    diffs[2] = null;
-    diffs[4] = null;
-    diffs[7] = null;
-  }
-
-  return diffs.filter(d => d !== null);
-}
 
 const mapStateToProps = state => ({
   board: state.board
